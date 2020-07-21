f27f6814b0a885fb92ba85efbba620d26236ff8b
Bruno Ochotorena <hackerhilt@gmail.com>
2020-07-21 13:07:31 -0300

    Adicionando components de container e grid

---
+++ src/components/Container.js
@@ -0,0 +1,7 @@
+import React from "react"
+import { ContainerWrapper } from "../elements"
+
+/** @type {React.FC} */
+export const Container = ({ children }) => (
+  <ContainerWrapper>{children}</ContainerWrapper>
+)
